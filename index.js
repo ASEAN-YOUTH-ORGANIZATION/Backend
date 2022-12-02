37b39670d9df6f283790bcaefc23c944c05a4b1e
chrisimora <70998302+chrisimora@users.noreply.github.com>
2022-12-02 22:28:07 +0700

    Add files via upload
    API BACKEND DONE

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ dotenv.config()
 const app = express()
 const PORT = 3001
 
-app.use(cors())
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'], credentials: true }))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(multer().any())
-
 app.use('/api', routers)
+
+app.use(express.static('public'))
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
